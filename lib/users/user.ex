ca8ef43cbdbbe5b77f21c21290d8f483a9ac96d5
NerdsMaxx <henrique101124@gmail.com>
2022-01-08 07:57:32 -0300

    atualizando

--- lib/users/user.ex
+++ lib/users/user.ex
@@ -1,9 +1,25 @@
 defmodule Flightex.Users.User do
-  @keys [:name, :email, :cpf, :id]
+  @keys [:id, :name, :cpf, :email]
   @enforce_keys @keys
   defstruct @keys
 
-  def build do
-    # TO DO
+  def build(name, cpf, email)
+  when is_bitstring(name)
+  and String.match?(name, ~r/^[[:alpha:][:blank:]]+$/u)
+  and is_bitstring(cpf)
+  and is_bitstring(email)
+  do
+    {:ok,
+      %__MODULE__{
+        id: UUID.uuid4(),
+        name: name,
+        cpf: cpf,
+        email: email
+      }
+    }
+  end
+
+  def build(_name, _cpf, _email) do
+    {:error, "User not created!"}
   end
 end
