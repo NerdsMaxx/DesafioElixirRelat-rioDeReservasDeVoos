e0e26e456e6cb19037bde1c4a0c89763baa469ee
NerdsMaxx <henrique101124@gmail.com>
2022-01-12 09:05:12 -0300

    melhorando o report

--- lib/bookings/report.ex
+++ lib/bookings/report.ex
@@ -1,6 +1,29 @@
 defmodule Flightex.Bookings.Report do
+  alias Flightex.Bookings.Booking
+  alias Flightex.Bookings.CreateOrUpdate, as: BookingCoU
+
   def create(filename \\ "report_bookings.csv") do
     File.write!(filename, "")
     {:ok, "File created!"}
+  end
+
+  def generate_report() do
+    BookingCoU.get_all()
+    |> Map.values()
+    |> Enum.map(&booking_str(&1))
+    |> Enum.each(&add_in_report(&1))
+  end
+
+  defp booking_str(%Booking{id: id,
+                            user_id: user_id,
+                            local_origin: local_origin,
+                            local_destination: local_destination,
+                            complete_date: complete_date})
+  do
+    "#{id}, #{user_id}, #{local_origin}, #{local_destination}, #{complete_date}\n"
+  end
+
+  defp add_in_report(report, filename \\ "report_bookings.csv") do
+    File.write!(filename, report)
   end
 end
