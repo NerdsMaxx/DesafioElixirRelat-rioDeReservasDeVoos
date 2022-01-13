d8848aff770d0c61cd2170a355fcce05f040d599
NerdsMaxx <henrique101124@gmail.com>
2022-01-12 22:04:55 -0300

    Finalizado o desafio

--- lib/bookings/report.ex
+++ lib/bookings/report.ex
@@ -2,13 +2,46 @@ defmodule Flightex.Bookings.Report do
   alias Flightex.Bookings.Booking
   alias Flightex.Bookings.CreateOrUpdate, as: BookingCoU
 
-  def generate(filename \\ "report_bookings.csv") do
-    File.write(filename, "")
+  def generate_all_reports(filename \\ "report_bookings.csv") do
+    create_file(filename)
 
     BookingCoU.get_all()
     |> Map.values()
     |> Enum.map(&booking_str(&1))
-    |> Enum.each(&add_in_report(&1,filename))
+    |> Enum.each(&add_in_report(&1, filename))
+
+    {:ok, "Report generated successfully"}
+  end
+
+  def generate_report(from_date, to_date, filename \\ "report_bookings_based_on_date.csv")
+    when is_struct(from_date, Date)
+    and is_struct(to_date, Date)
+  do
+    create_file(filename)
+
+    BookingCoU.get_all()
+    |> Map.values()
+    |> Enum.filter(&filter_based_on_date(&1, from_date, to_date))
+    |> Enum.map(&booking_str(&1))
+    |> Enum.each(&add_in_report(&1, filename))
+
+    {:ok, "Report generated successfully"}
+  end
+
+  defp create_file(filename) do
+    File.write(filename, "")
+  end
+
+  defp filter_based_on_date(%Booking{complete_date: complete_date}, from_date, to_date) do
+    complete_date
+    |> NaiveDateTime.to_date()
+    |> compare_date(from_date, to_date)
+  end
+
+  defp compare_date(date, from_date, to_date) do
+    compare1 = Date.compare(date, from_date)
+    compare2 = Date.compare(date, to_date)
+    (compare1 == :gt or compare1 == :eq) and (compare2 == :lt or compare2 == :eq)
   end
 
   defp booking_str(%Booking{user_id: user_id,
@@ -28,7 +61,7 @@ defmodule Flightex.Bookings.Report do
   #   "#{id}, #{user_id}, #{local_origin}, #{local_destination}, #{complete_date}\n"
   # end
 
-  defp add_in_report(report, filename \\ "report_bookings.csv") do
+  defp add_in_report(report, filename) do
     File.write!(filename, report, [:append])
   end
 end
--- lib/flightex.ex
+++ lib/flightex.ex
@@ -1,5 +1,6 @@
 defmodule Flightex do
   alias Flightex.Bookings.CreateOrUpdate, as: BookingCoU
+  alias Flightex.Bookings.Report, as: BookingReport
   alias Flightex.Users.CreateOrUpdate, as: UserCoU
 
   def start_agents() do
@@ -10,15 +11,19 @@ defmodule Flightex do
   #Este é para teste
   defdelegate create_or_update_booking(params), to: BookingCoU, as: :call
 
-  defdelegate addUser(params), to: UserCoU, as: :add
-  defdelegate addBooking(params), to: BookingCoU, as: :add
+  defdelegate add_user(params), to: UserCoU, as: :add
+  defdelegate add_booking(params), to: BookingCoU, as: :add
 
-  defdelegate getUser(user_id), to: UserCoU, as: :get
-  defdelegate getBooking(booking_id), to: BookingCoU, as: :get
+  defdelegate get_user(user_id), to: UserCoU, as: :get
+  defdelegate get_booking(booking_id), to: BookingCoU, as: :get
 
-  defdelegate getAllUsers(), to: UserCoU, as: :get_all
-  defdelegate getAllBookings(), to: BookingCoU, as: :get_all
+  defdelegate get_all_users(), to: UserCoU, as: :get_all
+  defdelegate get_all_bookings(), to: BookingCoU, as: :get_all
 
-  defdelegate removeUser(user_id), to: UserCoU, as: :remove
-  defdelegate removeBooking(booking_id), to: BookingCoU, as: :remove
+  defdelegate remove_user(user_id), to: UserCoU, as: :remove
+  defdelegate remove_booking(booking_id), to: BookingCoU, as: :remove
+
+  defdelegate generate_all_reports(filename), to: BookingReport, as: :generate_all_reports
+  defdelegate generate_report(from_date, to_date), to: BookingReport, as: :generate_report
+  defdelegate generate_report(from_date, to_date, filename), to: BookingReport, as: :generate_report
 end
